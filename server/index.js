5e3c20578fe2c8209f37c7da9417aacfe41bb5fb
Umang Patel <umangpatel1991@gmail.com>
2023-03-30 15:31:13 -0400

    get a todo by id

--- server/index.js
+++ server/index.js
@@ -37,6 +37,21 @@ app.get('/todos', async (req, res) => {
   }
 });
 
+//get a todo
+
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM perntodo WHERE todo_id = $1', [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 app.listen(3000, () => {
   console.log('server has started on port 3000');
 });
