c22cd652339c25fd3f322d92a3205ca4437e5819
Umang Patel <umangpatel1991@gmail.com>
2023-03-30 07:34:54 -0400

    use cors middleware

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
 
 app.listen(3000, () => {
   console.log('server has started on port 3000');
