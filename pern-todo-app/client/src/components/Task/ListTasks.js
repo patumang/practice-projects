c203d43b9775b96a62ca6f76f67ceed866c383fd
Umang Patel <umangpatel1991@gmail.com>
2023-04-13 21:13:37 -0400

    add mui styles to list tasks

--- pern-todo-app/client/src/components/Task/ListTasks.js
+++ pern-todo-app/client/src/components/Task/ListTasks.js
@@ -1,10 +1,47 @@
+import * as React from 'react';
 import { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Grid from '@mui/material/Grid';
+
+import { yellow } from '@mui/material/colors';
+import SearchIcon from '@mui/icons-material/Search';
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: yellow[600],
+    },
+  },
+});
+
+const columns = [
+  { id: 'task_title', label: 'Task Title', minWidth: 500 },
+  { id: 'edit', label: 'Edit', minWidth: 50 },
+  { id: 'view', label: 'View', minWidth: 50 },
+];
 
 const ListTasks = () => {
   const [searchKeyword, setSearchKeyword] = useState('');
   const [searchField, setSearchField] = useState('all');
   const [tasks, setTasks] = useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   const getTasks = async () => {
     try {
@@ -27,62 +64,111 @@ const ListTasks = () => {
     return () => clearTimeout(timer);
   }, [searchKeyword, searchField]);
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
     <Fragment>
-      <form className='form-inline'>
-        <div className='form-group'>
-          <input
-            type='text'
-            className='form-control'
-            id='keywordSearch'
-            placeholder='Search'
-            value={searchKeyword}
-            onChange={(e) => setSearchKeyword(e.target.value)}
+      <ThemeProvider theme={theme}>
+        <Box sx={{ mt: 1, mx: 'auto', maxWidth: '95%' }}>
+          <Grid container spacing={2} alignItems='flex-end' sx={{ my: 3 }}>
+            <Grid item xs={8} md={10}>
+              <FormControl fullWidth variant='standard'>
+                <TextField
+                  id='keywordSearch'
+                  label='Search'
+                  value={searchKeyword}
+                  onChange={(e) => setSearchKeyword(e.target.value)}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position='start'>
+                        <SearchIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                  variant='standard'
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={4} md={2}>
+              <FormControl fullWidth size='small'>
+                <Select
+                  id='searchCategory'
+                  value={searchField}
+                  onChange={(e) => setSearchField(e.target.value)}
+                >
+                  <MenuItem value={'all'}>All</MenuItem>
+                  <MenuItem value={'title'}>Title</MenuItem>
+                  <MenuItem value={'description'}>Description</MenuItem>
+                  <MenuItem value={'tag'}>Tag</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Box>
+        <Box sx={{ mt: 1, mx: 'auto', maxWidth: '95%' }}>
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label='sticky table'>
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tasks
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((task) => {
+                    return (
+                      <TableRow
+                        hover
+                        role='checkbox'
+                        tabIndex={-1}
+                        key={task.task_id}
+                      >
+                        <TableCell>{task.task_title}</TableCell>
+                        <TableCell>
+                          <Link to={`/tasks/${task.task_id}/update`}>
+                            <Button variant='contained' color='secondary'>
+                              Edit
+                            </Button>
+                          </Link>
+                        </TableCell>
+                        <TableCell>
+                          <Link to={`/tasks/${task.task_id}`}>
+                            <Button variant='contained'>View</Button>
+                          </Link>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25, 100]}
+            component='div'
+            count={tasks.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
           />
-        </div>
-        <div className='form-group'>
-          <select
-            className='select form-control ml-2'
-            value={searchField}
-            onChange={(e) => setSearchField(e.target.value)}
-          >
-            <option key='all' value='all'>
-              All
-            </option>
-            <option key='title' value='title'>
-              Title
-            </option>
-            <option key='description' value='description'>
-              Description
-            </option>
-            <option key='tag' value='tag'>
-              Tag
-            </option>
-          </select>
-        </div>
-      </form>
-      <table className='table mt-5 text-center'>
-        <thead>
-          <tr>
-            <th>Task Title</th>
-            <th>Edit</th>
-            <th>View</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => (
-            <tr key={task.task_id}>
-              <td>{task.task_title}</td>
-              <td>
-                <Link to={`/tasks/${task.task_id}/update`}>Edit</Link>
-              </td>
-              <td>
-                <Link to={`/tasks/${task.task_id}`}>View</Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        </Box>
+      </ThemeProvider>
     </Fragment>
   );
 };
